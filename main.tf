854167861d6d4a3589b1209f1bc62b1c0473578c
Nicklaus McClendon <nick.mcclendon@credera.com>
2019-09-10 15:57:04 -0500

    Add dummy role

--- main.tf
+++ main.tf
@@ -23,6 +23,27 @@ resource "aws_iam_role" "iam_for_lambda" {
 EOF
 }
 
+
+resource "aws_iam_role" "iam_for_dummy" {
+  name = "iam_for_dummy"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
 resource "aws_lambda_function" "test_lambda" {
   function_name = "lambda_function_name"
   role          = "${aws_iam_role.iam_for_lambda.arn}"
